13d4b00ff576c1a65c5949846ab2137972488ded
Magdaléna Pazlarová <m.pazlarova@gmail.com>
2021-04-29 19:11:48 +0200

    buttons barvy v css, základy javascriptu

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 'use strict';
 
 console.log('funguju');
+
+const kosileBila = document.getElementById('bila');
+const kosileCervena = document.getElementById('cervena');
+const kosileCerna = document.getElementById('cerna');
+const kosileModra = document.getElementById('modra');
+const kosile = document.getElementById('product-image');
+const objednat = document.getElementById('objednat');
+const productColors = document.querySelector('.product__colors');
+
+// kosileBila.addEventListener('click', console.log('bilá klik'));
+
+kosileBila.addEventListener('click', function () {
+  kosile.style = 'fill: #ffffff';
+});
+kosileCerna.addEventListener('click', function () {
+  kosile.style = 'fill: #242222';
+});
+kosileCervena.addEventListener('click', function () {
+  kosile.style = 'fill: #a32424';
+});
+kosileModra.addEventListener('click', function () {
+  kosile.style = 'fill: #087596';
+});
+
+objednat.addEventListener('click', function () {
+  productColors.innerHTML = `<p>Objednáno</p>`;
+});
